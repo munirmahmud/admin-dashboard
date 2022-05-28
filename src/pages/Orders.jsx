733acc02e7532df0674824082f3c6187c7c09f85
Munir Mahmud <munirmahmud6@gmail.com>
2022-05-28 14:01:49 +0600

    Data table for the Orders done

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,9 +1,50 @@
+import {
+  ColumnDirective,
+  ColumnsDirective,
+  ContextMenu,
+  Edit,
+  ExcelExport,
+  Filter,
+  GridComponent,
+  Inject,
+  Page,
+  PdfExport,
+  Resize,
+  Sort,
+} from "@syncfusion/ej2-react-grids";
 import { Header } from "../components";
+import { ordersData, ordersGrid } from "../helpers";
 
 const Orders = () => {
   return (
-    <div>
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
       <Header category="Page" title="Orders" />
+
+      <GridComponent
+        id="gridcomp"
+        dataSource={ordersData}
+        allowPaging
+        allowSorting
+      >
+        <ColumnsDirective>
+          {ordersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+
+        <Inject
+          services={[
+            Resize,
+            Sort,
+            ContextMenu,
+            Filter,
+            Page,
+            ExcelExport,
+            Edit,
+            PdfExport,
+          ]}
+        />
+      </GridComponent>
     </div>
   );
 };
