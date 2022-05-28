5812f48905bfcdf370ec170d23849fdc62b527e1
Munir Mahmud <munirmahmud6@gmail.com>
2022-05-28 13:41:05 +0600

    Button & Sparkline chart done

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,5 +1,36 @@
-const SparkLine = () => {
-  return <div>SparkLine</div>;
+import {
+  Inject,
+  SparklineComponent,
+  SparklineTooltip,
+} from "@syncfusion/ej2-react-charts";
+
+const SparkLine = ({ currentColor, id, type, height, width, data, color }) => {
+  console.log(data);
+  return (
+    <SparklineComponent
+      id={id}
+      height={height}
+      width={width}
+      lineWidth={1}
+      valueType="Numeric"
+      fill={color}
+      border={{ color: currentColor, width: 2 }}
+      dataSource={data}
+      xName="x"
+      yName="yval"
+      type={type}
+      tooltipSettings={{
+        visible: true,
+        // eslint-disable-next-line no-template-curly-in-string
+        format: "${x} : data ${yval}",
+        trackLineSettings: {
+          visible: true,
+        },
+      }}
+    >
+      <Inject services={[SparklineTooltip]} />
+    </SparklineComponent>
+  );
 };
 
 export default SparkLine;
