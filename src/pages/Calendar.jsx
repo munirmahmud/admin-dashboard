c9c61bcb5571247119f2e82fa70bd69c0a9c7919
Munir Mahmud <munirmahmud6@gmail.com>
2022-05-28 14:47:36 +0600

    Calendar & kanban done

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,5 +1,33 @@
+import {
+  Agenda,
+  Day,
+  DragAndDrop,
+  Inject,
+  Month,
+  Resize,
+  ScheduleComponent,
+  Week,
+  WorkWeek,
+} from "@syncfusion/ej2-react-schedule";
+import Header from "../components/Header";
+import { scheduleData } from "../helpers";
+
 const Calendar = () => {
-  return <div>Calendar</div>;
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="App" title="Calendar" />
+
+      <ScheduleComponent
+        height="650px"
+        eventSettings={{ dataSource: scheduleData }}
+        selectedDate={new Date(2021, 0, 10)}
+      >
+        <Inject
+          services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]}
+        />
+      </ScheduleComponent>
+    </div>
+  );
 };
 
 export default Calendar;
